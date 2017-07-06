a3e9cefa0a6fce65f8553e88d8fc0ea8b582e8a7
moakem <moa13007@byui.edu>
2017-07-05 19:17:59 -0600

    background slider on gallery section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+var bgImageArray = ["aj-garcia-253576.jpg", "elk.jpeg", "matt-ragland-82514.jpg"],
+base = "images/",
+secs = 8;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.getElementById("gallery").style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.getElementById("gallery").style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
